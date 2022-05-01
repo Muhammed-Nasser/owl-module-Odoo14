8798731de52d14d8a437b335560b1fdfd7729ca1
Muhammed Nasser Shalaby Alazaly <alazaly@outlook.com>
2022-05-01 02:58:27 +0200

    final code

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "owl",
+    'summary': "an example module for OWL",
+    'description': """an example module for OWL""",
+    'author': "Muhamed Nasser",
+    'website': "http://www.example.com",
+    'category': '',
+    'version': '14.0.1',
+    'depends': [
+        'base',
+        'sale',
+        'sale_management',
+    ],
+    'data': [
+        'assets.xml',
+        'views.xml',
+    ],
+    'qweb': [
+    'static/src/js/components/PartnerOrderSummary.xml',
+    ]
+}
